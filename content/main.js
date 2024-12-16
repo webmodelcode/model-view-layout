edcb16c18654053c97a7a0752ff3680ed7ee770a
jpleon <juanleon8581@gmail.com>
2024-12-16 15:03:44 -0500

    add imports of scElements and strings to main.js

--- content/main.js
+++ content/main.js
@@ -1,3 +1,18 @@
-import { testExport } from "./utils/test.js";
+import {
+  getScBroadcastContainer,
+  getScBroadcastWrapper,
+  getScErrorNode,
+  getScExtSwitch,
+  getScMemberList,
+} from "./sc-components/sc-components-getters.js";
+import { strings } from "./utils/strings.js";
 
-testExport();
+console.log({ strings });
+
+console.log(
+  getScBroadcastContainer,
+  getScBroadcastWrapper,
+  getScErrorNode,
+  getScExtSwitch,
+  getScMemberList
+);
