24d977e311804478678d1049817d981c0365347c
jpleon <juanleon8581@gmail.com>
2024-12-23 12:49:24 -0500

    Add donate and move chat buttons to menu component

--- content/menu/menu.js
+++ content/menu/menu.js
@@ -2,6 +2,8 @@ import { sctMenuContainers } from "./components/containers/view.container.js";
 import { sctMenuAccordion } from "./components/accordion/view.accordion.js";
 import { sctStatusIndicator } from "./components/status-indicator/view.status-indicator.js";
 import { btnFocusChat } from "./components/buttons/btn-focus-chat/btn-focus-chat.view.js";
+import { btnMoveChat } from "./components/buttons/btn-move-chat/btn-move-chat.view.js";
+import { btnDonate } from "./components/buttons/btn-donate/btn-donate.view.js";
 
 export const sctMenu = () => {
   const create = () => {
@@ -9,6 +11,8 @@ export const sctMenu = () => {
     const accordion = sctMenuAccordion();
     const statusIndicator = sctStatusIndicator();
     const focusChatButton = btnFocusChat();
+    const moveChatButton = btnMoveChat();
+    const donateButton = btnDonate();
 
     const menu = containers.fixedContainer;
     const internalContainer = containers.internalContainer;
@@ -23,6 +27,8 @@ export const sctMenu = () => {
     statusIndicatorContainer.appendChild(statusIndicator.dot);
     statusIndicatorContainer.appendChild(statusIndicator.text);
     contentContainer.appendChild(focusChatButton);
+    contentContainer.appendChild(moveChatButton);
+    contentContainer.appendChild(donateButton);
 
     document.body.appendChild(menu);
   };
