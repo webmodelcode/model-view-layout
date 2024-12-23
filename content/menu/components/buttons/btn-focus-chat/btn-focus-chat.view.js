ca94b0e9aad262e7a59143b0f81aee69e1bdf0d5
jpleon <juanleon8581@gmail.com>
2024-12-23 12:27:46 -0500

    Refactor chat button component to use SVG icon and improve structure

--- content/menu/components/buttons/btn-focus-chat/btn-focus-chat.view.js
+++ content/menu/components/buttons/btn-focus-chat/btn-focus-chat.view.js
@@ -1,11 +1,25 @@
 export const btnFocusChat = () => {
-  const btnImage = document.createElement("img");
+  const btn = document.createElement("button");
+  btn.classList.add("sct-button");
+  btn.innerHTML = svgImg;
 
-  btnImage.alt = "Maximize chat";
-
-  const btnFocusChat = document.createElement("button");
-  btnFocusChat.classList.add("sct-button");
-  btnFocusChat.appendChild(btnImage);
-
-  return btnFocusChat;
+  return btn;
 };
+
+const svgImg = `<svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-maximize-2"
+    >
+      <polyline points="15 3 21 3 21 9" />
+      <polyline points="9 21 3 21 3 15" />
+      <line x1="21" x2="14" y1="3" y2="10" />
+      <line x1="3" x2="10" y1="21" y2="14" />
+    </svg>`;
