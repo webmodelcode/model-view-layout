d8a56ff45972f9e52a803911e06ddd2dbd77a08c
Juan Pablo Leon <juanleon8581@gmail.com>
2024-12-26 20:31:34 -0500

    Implement moveChatButton functionality with floating chat management

--- content/menu/components/buttons/btn-move-chat/btn-move-chat.controller.js
+++ content/menu/components/buttons/btn-move-chat/btn-move-chat.controller.js
@@ -1,6 +1,43 @@
+import { strings } from "../../../../utils/strings.js";
+
 export const moveChatButtonController = ({
   moveChatButton,
   getScChatContainer,
   checkAvaliableElements,
   FloatingElement,
-}) => {};
+}) => {
+  moveChatButton.addEventListener("click", () => {
+    const chat = getScChatContainer();
+    const floatingChatButton = new FloatingElement(chat, 9999, true);
+
+    if (!checkAvaliableElements()) {
+      window.alert(strings.elementsNotAvailable);
+      return;
+    }
+
+    if (!floatingChatButton.isActive()) {
+      doChatFloating({ floatingChatButton });
+      moveChatButton.classList.add("active");
+      return;
+    }
+
+    stopChatFloating({
+      getScChatContainer,
+      floatingChatButton,
+    });
+    moveChatButton.classList.remove("active");
+  });
+};
+
+const doChatFloating = ({ floatingChatButton }) => {
+  floatingChatButton.enable(floatingChatButton);
+  floatingChatButton.element?.classList.add("custom-floating-chat");
+};
+
+const stopChatFloating = ({ floatingChatButton }) => {
+  floatingChatButton.disable(floatingChatButton);
+  if (floatingChatButton.element) {
+    floatingChatButton.element.style = "";
+    floatingChatButton.element.classList.remove("custom-floating-chat");
+  }
+};
